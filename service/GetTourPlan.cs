b0c5c5fac8aa8e9734c0163e9ec53b8e337b13c0
tobias odion <tobiasodion@gmail.com>
2023-08-19 09:47:58 +0200

    Rename itourplanner- service to service. Update deployment workflow yml

--- service/GetTourPlan.cs
+++ service/GetTourPlan.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
-namespace ITourParis.Function
+namespace ITourPlanner.Service
 {
     public class GetTourPlan
     {
