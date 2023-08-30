b53df80b7d2a08271f7415220819acac67ec054d
SmartTobi <tobiasodion@gmail.com>
2023-08-30 22:39:58 +0200

    Add coming soon page (#5)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import logo from './itourparis-logo.svg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coming-soon">
+      <div className="content">
+        <img src={logo} className="logo" alt="iTourParis Logo" />
+        <h1 className="custom-font">Coming Soon</h1>
+        <p>We're working hard to bring you something amazing.</p>
+      </div>
     </div>
   );
 }
