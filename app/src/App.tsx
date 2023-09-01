0159873584298c6695296f1a1644bf2f19ad8040
SmartTobi <tobiasodion@gmail.com>
2023-09-02 00:02:04 +0200

    Add react bootstrap (#8)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,19 @@
+import React from 'react';
 import logo from './itourparis-logo.svg'
 import './App.css';
+import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
-    <div className="coming-soon">
-      <div className="content">
-        <img src={logo} className="logo" alt="iTourParis Logo" />
-        <h1 className="custom-font">Coming Soon</h1>
-        <p>We're working hard to bring you something amazing.</p>
+    <Container className="p-3">
+      <div className="coming-soon">
+        <div className="content">
+          <img src={logo} className="logo" alt="iTourParis Logo" />
+          <h1 className="custom-font">Coming Soon</h1>
+          <p>We're working hard to bring you something amazing.</p>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 }
 
