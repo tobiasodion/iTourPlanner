2058455cd21ff9239fbbe0de9733a7103436866f
tobias odion <tobiasodion@gmail.com>
2023-09-30 02:23:31 +0200

    Integrate recommendation app with openAI gpt3

---
+++ app/src/services/recommender-service.ts
@@ -0,0 +1,28 @@
+// openaiService.ts
+import axios from 'axios';
+
+// Function to call the OpenAI completion endpoint
+export const getCompletion = async (prompt: string) => {
+    // Replace 'YOUR_API_KEY' with your actual OpenAI API key
+    const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+
+    const openai = axios.create({
+        baseURL: 'https://api.openai.com/v1',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${OPENAI_API_KEY}`,
+        },
+    });
+
+    try {
+        const response = await openai.post('/completions', {
+            model: "text-davinci-003",
+            prompt: prompt,
+            max_tokens: 700
+        });
+        return response.data.choices[0].text;
+    } catch (error) {
+        console.error('Error calling OpenAI API:', error);
+        throw error;
+    }
+};
